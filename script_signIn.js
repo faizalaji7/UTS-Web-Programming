c163d50e9e1bf792132d2ca3f9363531b94d9c85
faizalaji7 <74334556+faizalaji7@users.noreply.github.com>
2020-11-12 09:02:10 +0700

    Add files via upload
    maaf web saya belum responsive wkwk

---
+++ script_signIn.js
@@ -0,0 +1,54 @@
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+});
+
+function checkInputs() {
+	const emailValue = email.value.trim();
+	const passwordValue = password.value.trim();
+	const password2Value = password2.value.trim();
+
+	if(emailValue === '') {
+		setErrorFor(email, 'Email tidak boleh kosong.');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Email yang anda masukkan salah.');
+	} else {
+		setSuccessFor(email);
+	}
+	
+	if(passwordValue === '') {
+		setErrorFor(password, 'Password tidak boleh kosong.');
+	} else {
+		setSuccessFor(password);
+	}
+	
+	if(password2Value === '') {
+		setErrorFor(password2, 'Password tidak boleh kosong.');
+	} else if(passwordValue !== password2Value) {
+		setErrorFor(password2, 'Password anda tidak cocok.');
+	} else{
+		setSuccessFor(password2);
+	}
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+	
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
